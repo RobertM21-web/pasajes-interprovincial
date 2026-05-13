1260a0307435dc2ca616d2f1cd5f62395b14c9f5
Alanbot-77 <yostinpenaloza1802@gmail.com>
2026-05-13 11:13:31 -0500

    refactor(cliente): integrar componente SelectorAsientos en la página

---
+++ app/cliente/selector-asientos/page.tsx
@@ -0,0 +1,9 @@
+import SelectorAsientos from "@/components/asientos/SelectorAsientos";
+
+export default function SelectorAsientosPage() {
+  return (
+    <div className="min-h-screen bg-gray-100 p-8">
+      <SelectorAsientos />
+    </div>
+  );
+}
